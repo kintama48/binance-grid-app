7c9fb72b1b5e96e8b8deb1388bc2830f79b55ecc
Abdullah Baig <rebelschaos@gmail.com>
2022-03-08 18:55:40 +0500

    latest

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+require("dotenv");
 
+let apiHandler = require("node-binance-api");
+let boughtPrice = null;
+let soldPrice = null;
+let profitGenerated = null;
+
+async function main(clientSecret, apiKey){
+
+
+}
+
+
+
+setInterval(async function(){
+    await main(process.env.CLIENT_SECRET, process.env.API_KEY);
+}, 10);
+
+console.log("Running... ");
