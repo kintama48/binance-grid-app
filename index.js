1493bfba1bcc075434b0309c2e41d8c26fbb0be3
Abdullah Baig <rebelschaos@gmail.com>
2022-03-08 19:28:02 +0500

    latest

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 require("dotenv");
 
 let apiHandler = require("node-binance-api");
-let boughtPrice = null;
-let soldPrice = null;
+
+console.log(`Running... \nCurrent price of Bitcoin: ${getCurrentPrice()}`)
+
+let sellPrice = prompt("Price to sell?\n");
+let buyPrice = prompt("Price to buy?\n");
+let stopLoss = prompt("Stop Loss?\\n");
+
 let profitGenerated = null;
 
 async function main(clientSecret, apiKey){
 
-
+    
 }
 
+async function getCurrentPrice(){
+
+}
 
 
 setInterval(async function(){
     await main(process.env.CLIENT_SECRET, process.env.API_KEY);
 }, 10);
 
-console.log("Running... ");
